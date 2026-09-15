0c79689112b21483934f587d8e744bf4f539a699
vaibhavmishra-dev <vaibhavmishra9679@gmail.com>
2026-09-15 22:44:59 +0530

    Refactor AuthProvider class structure

--- frontend/auth_provider.dart
+++ frontend/auth_provider.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
-
 import '../models/user_model.dart';
-
 import '../services/api_service.dart';
-
 class AuthProvider extends ChangeNotifier {
-  
   final ApiService _api = ApiService();
-  
   UserModel? user;
-  
-  String? token;
-  
+  String? token; 
   bool _isAuthenticated = false;
-
   bool get isAuthenticated => _isAuthenticated;
-
   Future<void> login(String email, String password) async {
-    
     final result = await _api.login(email, password);
-    
     user = UserModel.fromJson(result['user']);
-    
     token = result['token'];
-    
     _isAuthenticated = true;
-    
     notifyListeners();
-    
   }
 
   
